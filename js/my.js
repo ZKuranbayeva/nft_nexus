a8a8f4c56777adea5d987e8ed8ced2e6c7032a35
ZKuranbayeva <132888673+ZKuranbayeva@users.noreply.github.com>
2023-08-25 20:29:16 +0500

    Initial commit

--- js/my.js
+++ js/my.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $('.menu__btn').on('click', function(){
         $('.menu__list-items').toggleClass('menu__list-items--active');
     });
+
+    $('.select').on('click',function(){
+        $('.about__menu-list ul').toggleClass('about__menu-list--active')
+    });
 });
